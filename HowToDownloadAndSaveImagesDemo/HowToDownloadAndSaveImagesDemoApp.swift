dc5d25e3411a6af017a7194c50110395053bf3e9
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 19:00:33 -0600

    Initial Commit

---
+++ HowToDownloadAndSaveImagesDemo/HowToDownloadAndSaveImagesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToDownloadAndSaveImagesDemoApp.swift
+//  HowToDownloadAndSaveImagesDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HowToDownloadAndSaveImagesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
